6c65ea190007d83935bc70b47cd2ae7d4e66c3b8
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-04-25 13:27:48 +0530

    Initial commit.
    Made simple Laravel app for a "Movies" table with CRUD operations. It uses Blade Templating right now.

---
+++ resources/views/movies/edit.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Edit Movie Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Edit Movie</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('movies.index') }}" enctype="multipart/form-data">
+                        Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('movies.update',$movie->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Movie Name:</strong>
+                        <input type="text" name="name" value="{{ $movie->name }}" class="form-control"
+                            placeholder="Movie name">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Producing Studio:</strong>
+                        <input type="text" name="studio" class="form-control" placeholder="Name of Studio"
+                            value="{{ $movie->studio }}">
+                        @error('studio')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Year of Release:</strong>
+                        <input type="text" name="yor" value="{{ $movie->year_of_release }}" class="form-control"
+                            placeholder="Year of Release">
+                        @error('yor')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Movies Index CRUD</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Laravel 9 CRUD Example Tutorial</h2>
+                </div>
+                <div class="pull-right mb-2">
+                    <a class="btn btn-success" href="{{ route('movies.create') }}"> Create Movie</a>
+                </div>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+        @endif
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Movie Name</th>
+                    <th>Producing Studio</th>
+                    <th>Year of Release</th>
+                    <th width="280px">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>{{ $movie->id }}</td>
+                        <td>{{ $movie->name }}</td>
+                        <td>{{ $movie->studio }}</td>
+                        <td>{{ $movie->year_of_release }}</td>
+                        <td>
+                            <form action="{{ route('movies.destroy',$movie->id) }}" method="Post">
+                                <a class="btn btn-primary" href="{{ route('movies.edit',$movie->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+            </tbody>
+        </table>
+        {!! $movies->links() !!}
+    </div>
+</body>
+</html>
