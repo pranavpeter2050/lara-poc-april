a48b8ba3b61a4f72b994c9f32958d64ac30e3009
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-04-27 10:13:39 +0530

    Calling theme related stylesheets and scripts in Blade file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,30 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Movies Index</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+        <!-- Daterangepicker css -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
+
+        <!-- Vector Map css -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}">
+
+        <!-- Theme Config Js -->
+        <script src="{{ asset('assets/js/hyper-config.js') }}"></script>
+
+        <!-- Icons css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="{{ asset('assets/css/app-modern.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
 
         <!-- Styles -->
         <style>
@@ -19,5 +38,24 @@
         <div id="app"></div>
 
         @vite('resources/js/app.js')
-    </body>
+            <!-- Vendor js -->
+            <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+
+            <!-- Dashboard App js -->
+            <script src="{{ asset('assets/js/pages/demo.dashboard.js') }}"></script>
+
+            <!-- App js -->
+            <script src="{{ asset('assets/js/app.min.js') }}"></script>
+
+            {{-- <!-- Daterangepicker js -->
+            <script src="assets/vendor/daterangepicker/moment.min.js"></script>
+            <script src="assets/vendor/daterangepicker/daterangepicker.js"></script>
+
+            <!-- Apex Charts js -->
+            <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+
+            <!-- Vector Map js -->
+            <script src="assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+            <script src="assets/vendor/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script> --}}
+        </body>
 </html>
